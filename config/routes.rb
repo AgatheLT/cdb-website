9c75469d19df336448bcb37a597c8f053f6bf2c2
Agathe Laburthe TOlra <agathe.laburthetolra@gmail.com>
2021-02-04 16:23:36 -0600

    create git

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :projects
+
+  get 'pages/contact'
+  get 'pages/cv'
+end
